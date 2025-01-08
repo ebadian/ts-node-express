f4aa39d99eb3ad4070776bcbbd61e87af2a8243f
ali.ebadian <ali.ebadian@gmail.com>
2025-01-08 10:16:53 +0000

    adding simple nunjucks hello world example

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 import express, { Express, Request, Response } from "express";
 
 import dotenv from "dotenv";
+import nunjucks from "nunjucks";
+import path from "path"
 
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+nunjucks.configure(path.join(__dirname, "views"),{
+    autoescape: true,
+    express:app,
+    watch: true
+});
+
+app.set("view engine", "njk");
+
+
 app.get("/", (req:Request, res:Response) => {
-    res.send("Hello")
+    const data = {
+        title: "Hello World",
+        message: "Welcome to the World!"
+    }
+    res.render("index.njk", data)
 })
 
 app.listen(port, () => {
